a2f5fe0fe2e9f0bb3b41bf825c1bfb1c5c43a17c
adriellyavelar <adriellyavelaar@gmail.com>
2024-06-02 21:12:22 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+while (c <= 10 ) {
+    console.log(`Passo ${c}`)
+    c++
+} 
+
+// Teste lógico no início
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10 )*/ 
+
+// Teste lógico no final
